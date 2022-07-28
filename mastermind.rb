36aeebb6fdbb27002e32cb3667f39632602c3923
IgnisTynka <marcia03835@gmail.com>
2022-07-28 21:29:06 +0200

    Concept

---
+++ mastermind.rb
@@ -0,0 +1,13 @@
+class Mastermind
+    def initialize
+    end
+end
+
+#need methot to  randomly selects the secret colors / gest cod from player / comparing the code with the cipher / puts feedback
+
+#ruls: good color good place / good color wrong place / wrong color
+
+#kroki 
+#1. Wybierasz czy chcesz byc osoba ktora tworzy kod czy ktora zgaduje kod
+#2.1 Jezeli wybrales tworzenie kodu podajesz 4 cyfry ktore sa kodem nastepnie odgaduje komputer
+#2.2 Jezeli zgadujesz to wprowadzasz 4 cyfry i dostajesz feedback zgodny z zasadami 
