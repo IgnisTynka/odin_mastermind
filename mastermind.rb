2d581dc44782d29e1c44b30a27756995654e3bf5
IgnisTynka <marcia03835@gmail.com>
2022-07-28 21:31:07 +0200

    Concept

--- mastermind.rb
+++ mastermind.rb
@@ -3,7 +3,7 @@ class Mastermind
     end
 end
 
-#need methot to  randomly selects the secret colors / gest cod from player / comparing the code with the cipher / puts feedback
+#need methot to  randomly selects the secret colors / gest cod from player / comparing the code with the cipher / puts feedback 
 
 #ruls: good color good place / good color wrong place / wrong color
 
@@ -11,3 +11,7 @@ end
 #1. Wybierasz czy chcesz byc osoba ktora tworzy kod czy ktora zgaduje kod
 #2.1 Jezeli wybrales tworzenie kodu podajesz 4 cyfry ktore sa kodem nastepnie odgaduje komputer
 #2.2 Jezeli zgadujesz to wprowadzasz 4 cyfry i dostajesz feedback zgodny z zasadami 
+
+#need find information about display colors in ruby 
+
+#need to make concept more more specific
