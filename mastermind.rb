6ba20769557eac64fd2d3eaec4ee0959e1d862ba
IgnisTynka <marcia03835@gmail.com>
2022-07-31 21:14:06 +0200

    Added method equal? and started main loop

--- mastermind.rb
+++ mastermind.rb
@@ -1,22 +1,26 @@
-class Mastermind
-    def initialize
+class Code
+    def initialize(code)
+        @code = code
+    end
+    def equal?(other)
+        return @code[0] == other[0] && @code[1] == other[1] && @code[2] == other[2] && @code[3] == other[3]
     end
 end
-
-#need methot to  randomly selects the secret colors / gest cod from player / comparing the code with the cipher / puts feedback 
-
-#ruls: good color good place / good color wrong place / wrong color
-
-#kroki 
-#1. Wybierasz czy chcesz byc osoba ktora tworzy kod czy ktora zgaduje kod
-#2.1 Jezeli wybrales tworzenie kodu podajesz 4 cyfry ktore sa kodem nastepnie odgaduje komputer
-#2.2 Jezeli zgadujesz to wprowadzasz 4 cyfry i dostajesz feedback zgodny z zasadami 
-
-#need find information about display colors in ruby 
-
-#need to make concept more more specific
 
 puts "Welcome to the game Mastermind! 
 You can be the code maker or the code breaker.
 Choose 1 if you want to be a code maker or 2 if you want to be a code breaker"
-role = gets.chomp.to_i - 1 
+role = gets.chomp.to_i 
+
+if role == 1
+    puts "tbd"
+elsif role == 2
+    mystery_code = Code.new("1234")
+    puts "Write 4 digits code"
+    player_code = gets.chomp
+
+    until mystery_code.equal?(player_code)
+        puts "Write 4 digits code"
+        player_code = gets.chomp  
+    end
+end
