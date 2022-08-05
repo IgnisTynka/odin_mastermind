d4d22525a15f4a61f82fa3f3ab84ad680921e7a2
IgnisTynka <marcia03835@gmail.com>
2022-08-05 11:54:21 +0200

    Added generate_code method

--- mastermind.rb
+++ mastermind.rb
@@ -1,19 +1,23 @@
 class Code
-    def initialize(code)
+    attr_accessor :code
+    def initialize(code = "")
         @code = code
     end
+    def generate_code
+        i = 0
+        while i < 4
+            digit = rand(10).to_s
+            puts digit
+            unless @code.include?(digit)
+                @code[i] = digit
+                i += 1
+            end 
+        end
+        @code 
+    end 
     def equal?(other)
         return @code[0] == other[0] && @code[1] == other[1] && @code[2] == other[2] && @code[3] == other[3]
     end
-    # def get_total_info(guess)
-    #     total_info = ""
-    #     i = 0
-    #     4.times do
-    #         total_info += mystery_code.get_info(i, guess[i])
-    #         i += 1
-    #     end
-    #     return total_info
-    # end
     def get_info(index, digit)
         if @code.include?(digit)
             if @code[index] == digit
@@ -23,6 +27,9 @@ class Code
             end
         end
         return "0"        
+    end
+    def get_code
+        @code
     end
 end
 
@@ -34,7 +41,8 @@ role = gets.chomp.to_i
 if role == 1
     puts "tbd"
 elsif role == 2
-    mystery_code = Code.new("1234")
+    mystery_code = Code.new(Code.new().generate_code)
+    puts mystery_code.get_code
     player_code = ""
     puts "Your mission is to guess the four-digit secret code. 
 After each attempt you will get information about how close you are according to the roles below:
